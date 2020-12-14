7ee047abc9b44e5a9ef0353ed7ae7cb6123758a8
kevmccall <kev_mccall@yahoo.com>
2020-12-14 01:59:04 -0700

    added styling, finished JS exercises

--- app.js
+++ app.js
@@ -74,21 +74,55 @@
 //Create an Array called "donuts" that list 7-10 different items
 //1 Using a loop, iterate through this array and console.log all the donuts
 let donuts= ['Glazed', 'Chocolate', 'Apple Fritter', 'Eclair', 'Cronut', 'Boston Cream', 'Maple Bar', 'Bear Claw'];
+
+for (i=0; i<donuts.length; i++) {
+    console.log(donuts[i],);
+    
+    }
+    
 //2 Write the command to remove the first donut fron the array
 donuts.shift();
-//3 Write the command to remove the last donus from the array
+//3 Write the command to remove the last donuts from the array
 donuts.pop();
 //4 Write the command to add a new donut "lime zest" to the front of the array
-donuts.splice(0, 0, "Lime Zest");
+donuts.unshift (0, 0, "Lime Zest");
 //5 Write the command to add another donut "lemon pie" to the end of the array
 donuts.splice(7, 0, "Lemon Pie");
 
+//6 Use either the join() or toString() method to convert an array to a string.
+donuts.join(",");
+
+//7 Write the command to make a copy of the array using slice. The copy should NOT include two donuts.
 donuts = donuts.slice(2);
 
+//8 Write the command that gives the indexOf where "chocolate" is located.
+// donuts.indexOf("Chocolate");
+donuts.indexOf("Chocolate");
 
-for (i=0; i<donuts.length; i++) {
-console.log(donuts[i],);
+//9 Write the command that gives the indexOf where "glazed" is located (You may get a negative number).
+donuts.indexOf("Glazed");
 
-}
+//10 **Using the splice command, remove one donut from the array and add another.
+donuts.splice(6, 1, "Sprinkle");
+
+
+
+
 console.log(donuts.join(","));
 console.log(donuts.slice(2));
+console.log(donuts.indexOf("Chocolate"));
+console.log(donuts.splice(6, 1, "Sprinkle"));
+
+//11 **Create a new variable called withSyed and set it equal to the people array concatenated with the string of "Bob".
+let withSyed = donuts.concat("Bob");
+console.log(donuts.concat("Bob"));
+
+
+//12 **Create a function called getdonuts() that will display all the donuts in your array and display on your page.
+function getdonuts() {
+    document.getElementById("types").innerHTML = donuts;
+    
+}
+
+getdonuts();
+
